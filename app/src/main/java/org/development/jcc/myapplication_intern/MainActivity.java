7115c34dc1d1fb1f05779f7c446480f255863c61
jolas013 <jolaslibres65@gmail.com>
2018-11-08 10:08:03 +0800

    Navigation View

--- app/src/main/java/org/development/jcc/myapplication_intern/MainActivity.java
+++ app/src/main/java/org/development/jcc/myapplication_intern/MainActivity.java
@@ -2,12 +2,23 @@ package org.development.jcc.myapplication_intern;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        addDrawerItems();
+    }
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 }
