25d55869402d3f0e8e5afd195153bf01ddef3162
jolas013 <jolaslibres65@gmail.com>
2018-11-09 17:32:24 +0800

    Login editext and button added

--- app/src/main/java/org/development/jcc/myapplication_intern/Main2Activity.java
+++ app/src/main/java/org/development/jcc/myapplication_intern/Main2Activity.java
@@ -1,13 +1,32 @@
 package org.development.jcc.myapplication_intern;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private EditText edituser;
+    private EditText editpass;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    edituser = (EditText)findViewById(R.id.user);
+    editpass = (EditText)findViewById(R.id.password);
+    btnLogin = (Button)findViewById(R.id.btnLogin);
+
+    btnLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Main2Activity.this,Main7Activity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
--- app/src/main/java/org/development/jcc/myapplication_intern/MainActivity.java
+++ app/src/main/java/org/development/jcc/myapplication_intern/MainActivity.java
@@ -12,26 +12,44 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerList = (ListView) findViewById(R.id.navList);
         addDrawerItems();
 
     }
+
     private void addDrawerItems() {
-        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        final String[] osArray = {"Login", "Register", "Logout", "Settings", "Email"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
         mDrawerList.setAdapter(mAdapter);
 
-    mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-            startActivity(intent);
-        }
-    });
-    }
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                } else if (position == 1) {
+                    Intent m2 = new Intent(MainActivity.this, Main3Activity.class);
+                    startActivity(m2);
+                } else if (position == 2) {
+                    Intent m3 = new Intent(MainActivity.this, Main4Activity.class);
+                    startActivity(m3);
+                } else if (position == 3) {
+                    Intent m4 = new Intent(MainActivity.this, Main5Activity.class);
+                    startActivity(m4);
+                } else if (position == 4) {
+                    Intent m5 = new Intent(MainActivity.this, Main6Activity.class);
+                    startActivity(m5);
+
+                }
+            }
+        });
 
     }
+}
