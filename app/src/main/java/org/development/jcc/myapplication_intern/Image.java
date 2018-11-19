fd469cb47a0620f9a95c88f9a70b4bee83c66fdd
jolas013 <jolaslibres65@gmail.com>
2018-11-19 11:50:06 +0800

    Input Stream added

---
+++ app/src/main/java/org/development/jcc/myapplication_intern/Image.java
@@ -0,0 +1,13 @@
+package org.development.jcc.myapplication_intern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
